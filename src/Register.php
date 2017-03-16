6ebc20d813f72e362bca1a299cc42dd6568ac7be
Farhan Wazir <farhan.wazir@gmail.com>
2017-03-16 18:03:01 +0500

    Force register excel dependency service provider.
    (cherry picked from commit 2e33e79)

--- src/Register.php
+++ src/Register.php
@@ -14,6 +14,7 @@ class Register
      * @return void
      */
     public function bootstrap(Application $app){
+        $app->register(new Maatwebsite\Excel\ExcelServiceProvider($app));
         $app->bind('MCMIS\Contracts\Exporter', 'MCMIS\Exporter\Container');
     }
 
